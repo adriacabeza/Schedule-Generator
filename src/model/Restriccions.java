5735fd2cc55354aace4de6217fb5062117e5a142
adriacabeza <nymeria82>
2018-11-02 00:05:55 +0100

    prueba pa ver si el user de git se me cambia

--- src/model/Restriccions.java
+++ src/model/Restriccions.java
@@ -5,12 +5,12 @@ import model.*;
 public abstract class Restriccions {
 
     int id;
-
-    private static Restriccions ourInstance = new Restriccions();
-    //TODO: preguntar a aina que coño e ezto i why dafuq cannot be abstract
-    public static Restriccions getInstance() {
-        return ourInstance;
-    }
+//
+//    private static Restriccions ourInstance = new Restriccions();
+//    //TODO: preguntar a aina que coño e ezto i why dafuq cannot be abstract
+//    public static Restriccions getInstance() {
+//        return ourInstance;
+//    }
 
     public Restriccions(int id) {
         this.id = id;
