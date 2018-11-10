1de699fd27e1f8b0d0518e45f9fa1868ec701f2b
zemone <antoni.rambla.moiset@gmail.com>
2018-11-11 00:41:37 +0100

    afegit mes restriccions

---
+++ src/model/RestriccioAula.java
@@ -0,0 +1,25 @@
+package model;
+
+import exceptions.NotFoundException;
+
+import java.util.ArrayList;
+
+public class RestriccioAula extends  Restriccions {
+
+    public RestriccioAula() {
+        super(3);
+
+    }
+
+    @Override
+    public boolean isable() throws NotFoundException {
+        return false;
+    }
+
+
+    public boolean isable( Aula aula, AssignaturaMonosessio assig) throws NotFoundException {
+        return (aula.getCapacitat() >= assig.getGrup().getCapacitat() && aula.getTipusAula() == assig.getSessio().gettAula());
+    }
+
+
+}
