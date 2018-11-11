b35144bb95830d21ca7448899ae2d4a8b3f4eb49
adriacabeza@hotmail.com <nymeria82>
2018-11-11 17:06:01 +0100

    mega json  d'assignatures creat, li he canviat el nom per si feu proves del driver no el borreu

--- src/model/RestriccioAulaHora.java
+++ src/model/RestriccioAulaHora.java
@@ -8,7 +8,12 @@ public class RestriccioAulaHora extends Restriccions {
     private int hora;
     private Aula aula;
 
-
+    /**
+     * Crea una restricció on es comprova si es pot realitzar una assignació en una determinada aula en una determinada hora d'un determinat dia
+     * @param dia dia a comprovar
+     * @param hora hora a comprovar
+     * @param aula aula a comprovar
+     */
     public RestriccioAulaHora(int dia, int hora, Aula aula) {
         super(7);
         this.dia = dia;
@@ -16,6 +21,13 @@ public class RestriccioAulaHora extends Restriccions {
         this.aula = aula;
     }
 
+    /**
+     * Retorna si es possible realitzar una assignació en una determinada aula en una determinada hora d'un determinat dia
+     * @param aula aula que es comprova
+     * @param dia dia que es comprova
+     * @param hora hora que es comprova
+     * @return true si es pot realitzar l'assignació
+     */
     public boolean isable(Aula aula, int dia,  int hora){
         if(aula.getKey() == this.aula.getKey() && dia == this.dia && hora == this.hora) return false;
         return true;
