a056eb423404a3371c10842942b6a35d88530e76
ainagarciaes <ainaa.105@gmail.com>
2018-11-26 20:04:56 +0100

    interface stuff

--- PROP-SEGONAENTREGA/src/views/Main.java
+++ PROP-SEGONAENTREGA/src/views/Main.java
@@ -12,10 +12,9 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setScene(new Scene(root, 800, 600));
         primaryStage.show();
     }
-
 
     public static void main(String[] args) {
         launch(args);
