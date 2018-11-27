a2ce850671e805b15cf576da61c5f9e0b3b3a831
ainagarciaes <ainaa.105@gmail.com>
2018-11-27 15:28:57 +0100

    jasdcjas

--- PROP-SEGONAENTREGA/src/views/Controller.java
+++ PROP-SEGONAENTREGA/src/views/Controller.java
@@ -1,4 +1,31 @@
 package views;
 
+import javafx.fxml.FXML;
+
 public class Controller {
+
+    @FXML
+    void mostraLlistaPlans() {
+
+    }
+
+    @FXML
+    void mostraLlistaAules() {
+
+    }
+
+    @FXML
+    void mostraLlistaAssignatures() {
+
+    }
+
+    @FXML
+    void mostraHorari() {
+
+    }
+
+    @FXML
+    void mostraInici() {
+
+    }
 }
--- PROP-SEGONAENTREGA/src/views/Main.java
+++ PROP-SEGONAENTREGA/src/views/Main.java
@@ -5,14 +5,16 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
+        primaryStage.setTitle("Generador d'horaris");
         primaryStage.setScene(new Scene(root, 800, 600));
+        primaryStage.setResizable(false);
         primaryStage.show();
     }
 
