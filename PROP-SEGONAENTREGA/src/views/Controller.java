055892f0307a9b22ad3b69b94636b4ac18acc2f1
ainagarciaes <ainaa.105@gmail.com>
2018-11-27 20:00:34 +0100

    Restructuring the GUI

--- PROP-SEGONAENTREGA/src/views/Controller.java
+++ PROP-SEGONAENTREGA/src/views/Controller.java
@@ -1,41 +1,68 @@
 package views;
 
+import controllers.CtrlDomini;
+import javafx.beans.InvalidationListener;
+import javafx.collections.FXCollections;
+import javafx.collections.ListChangeListener;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
 import javafx.scene.layout.BorderPane;
-import model.Laboratori;
+
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
+import java.util.ListIterator;
 
 public class Controller {
 
     @FXML
-    Button new_assig = new Button();
+    Label tab_title = new Label();
     @FXML
-    Label assig_label = new Label();
+    BorderPane content_tab = new BorderPane();
+    @FXML
+    BorderPane inner_content = new BorderPane();
+    @FXML
+    Button plus_button = new Button();
 
+    CtrlDomini controladorDomini;
+
+    public void initialize(){
+        mostraInici();
+        controladorDomini = CtrlDomini.getInstance();
+    }
 
     @FXML
     void mostraLlistaPlans() {
-
+        tab_title.setText("Llista de plans d'estudi");
+        ListView<String> llistaPlans = new ListView<>();
+        ObservableList<String> plans = FXCollections.observableArrayList("Pla1", "Pla2");
+        llistaPlans.setItems(plans);
+        inner_content.setCenter(llistaPlans);
     }
 
     @FXML
     void mostraLlistaAules() {
-
+        tab_title.setText("Llista d'aules");
+        inner_content.setCenter(null);
     }
 
     @FXML
     void mostraLlistaAssignatures() {
-
+        tab_title.setText("Llista d'assignatures");
+        inner_content.setCenter(null);
     }
 
     @FXML
     void mostraHorari() {
-        assig_label.setText("Titimu preciosi");
+
     }
 
     @FXML
     void mostraInici() {
-
+        tab_title.setText("Inici");
+        inner_content.setCenter(new Label("Benvinguts!"));
     }
 }
