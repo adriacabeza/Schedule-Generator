4f077113851e7cef0cb458017a4686f7bf43aca6
adriacabeza <adriacabeza@hotmail.com>
2018-12-08 14:10:41 +0100

    part dels todos que hi havia a horari

--- PROP-SEGONAENTREGA/src/model/Horari.java
+++ PROP-SEGONAENTREGA/src/model/Horari.java
@@ -5,12 +5,23 @@ package model;
 
 import exceptions.NotFoundException;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.HashMap;
 
 public class Horari {
 
     private Assignacio[][][] horari;
+    private RestriccioAula resAul;
+    private RestriccioGrupTeo resTeo;
+    private RestriccioSubgrupLab resSub;
+    private ArrayList<RestriccioAulaDia> resAula;
+    private ArrayList<RestriccioAulaHora> resAulaHora;
+    private ArrayList<RestriccioAssigMatiTarda> resMatiTarda;
+    private RestriccioCapacitatAula resCapAul;
+    private RestriccioCorrequisit resCorr;
+    private RestriccioLimits resLim;
+    private RestriccioNivell resNiv;
 
     /**
      * Obtenir l'horari
@@ -19,6 +30,14 @@ public class Horari {
      */
     public Assignacio[][][] getHorari() {
         return horari;
+    }
+
+
+    /**
+     * Constructora que et crea un horari buit
+     */
+    public Horari(){
+        horari = new Assignacio[][][];
     }
 
 
@@ -35,30 +54,29 @@ public class Horari {
      * @param resAulDia    conjunt de restriccions d'una aula en un dia
      * @param resAulaHora  conjunt de restriccions d'una aula en una hora
      * @param resMatiTarda conjunt de restriccions d'una assignatura de matins o tardes
+     * @param resCapAul    restricció de capacitat d'una aula
+     * @param resLim       restricció de límits d'hores
+     * @return true si es pot construir l'horari i false si no es pot
      */
-    //TODO horari creadora sin llamar a backtracking, solo lo crea vacio
-    /*
-    rollo que no se si se entiende: cuando se llame a generar horari, se hara sobre el horario vacio, es decir, haced una funcion aqui que sea
-    del estilo bool generarHorari(assignatures, aules, ...) que es la que se llamara desde el ctrlDomini sobre el
-    horario vacio anteriormente creado. La constructora no deberia tener ningun parametro
+/*
+    TLDR:
 
-    TLDR: - constructora sin parametros pls
-          - funcion generar horario que haga lo que hay en la constructora y haga return de un bool si se ha podido generar o no
           - las restricciones deberian ser un atributo del horario, no pasadas cada vez como parametro, desde dominio te pasare
             los bools de las restricciones que se tengan que crear (leidos desde la view) y tu te puedes hacer una funcion que te
             las de de alta todas para tenerlas preparadas para generar horario mas adelante
      */
-    public Horari( HashMap<String, Assignatura> assignatures, HashMap<String, Aula> aules, RestriccioCorrequisit resCorr, RestriccioNivell resNiv, RestriccioAula resAul, RestriccioGrupTeo resTeo,
+    boolean ConstruirHorari( HashMap<String, Assignatura> assignatures, HashMap<String, Aula> aules, RestriccioCorrequisit resCorr, RestriccioNivell resNiv, RestriccioAula resAul, RestriccioGrupTeo resTeo,
                   RestriccioSubgrupLab resSub, ArrayList<RestriccioAulaDia> resAulDia, ArrayList<RestriccioAulaHora> resAulaHora, ArrayList<RestriccioAssigMatiTarda> resMatiTarda, RestriccioCapacitatAula resCapAul, RestriccioLimits resLim ) {
 
         Algorismes algoritme = new Backtracking2(assignatures,aules,resCorr,resNiv,resAul,resTeo,resSub,resAulDia,resAulaHora,resMatiTarda,resCapAul,resLim);
         try {
             boolean b = ((Backtracking2) algoritme).generaHorari();
-            if (!b) System.out.println("No es pot fer l'horari");
+            if (!b) return false;
         } catch (NotFoundException e) {
             e.printStackTrace();
         }
         horari = algoritme.getHorari();
+        return true;
     }
 }
 
